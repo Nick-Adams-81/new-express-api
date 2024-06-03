fd4608b2eb4b9120eedc85319f13e8cc5239b7d1
Nicholas Adams <nicholasadams@Owners-MBP.lan>
2024-06-03 16:01:02 -0700

    added public folder and basic limiting to routes

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
+const port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-    res.send("Home");
+// set up static folder
+// app.use(express.static(path.join(__dirname, "public")));
+let  posts = [
+    {id: 1, title: "post one"},
+    {id: 2, title: "post two"},
+    {id: 3, title: "post three"}
+];
+
+
+app.get("/api/posts", (req, res) => {
+    const limit = Number(req.query.limit);
+    if(!isNaN(limit) && limit > 0) {
+        res.json(posts.slice(0, limit));
+    } else {
+        res.json(posts);
+    }
+    
 });
 
-const port = process.env.PORT || 8000;
+// get one postr by id
+app.get("/api/post/:id", (req, res, next) => {
+    const id = Number(req.params.id);
+    res.json(posts.filter((post) => post.id === id));
+})
+
+
 app.listen(port, () => console.log(`Server running on port: ${port}`));
