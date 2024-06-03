08469842be13e7d4cb08884bf42b01084f7dfe08
Nicholas Adams <nicholasadams@Owners-MBP.lan>
2024-06-03 15:37:45 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Home");
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server running on port: ${port}`));
